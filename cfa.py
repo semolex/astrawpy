342a613e05cf6c264d742ed10c8173a8cda4f219
semolex <semolex@live.com>
2019-12-26 23:46:45 +0200

    Initial commit

---
+++ cfa.py
@@ -0,0 +1,39 @@
+import rawpy
+import numpy as np
+
+import echo
+
+DEFAULT_NUM_OF_PLANES = 3
+
+
+class CFA:
+    """
+    Wraps operations with CFA (Color Filter Array).
+    """
+    def __init__(self, raw: rawpy._rawpy.RawPy):
+        self.raw = raw
+
+    def get_pattern(self):
+        """Returns Bayer/X-Trans pattern of the matrix (if possible). """
+        echo.info('Getting raw CFA pattern...')
+        return self.raw.raw_pattern
+
+    def extract_cfa(self, fill=0):
+        """
+        Extracts color planes from the raw file.
+        Pixels from other planes will be filled with passed `fill` param.
+
+        :param fill: value that will be used to fill pixel values.
+        """
+
+        echo.info(f'Value to fill: {fill}')
+        number_of_planes = self.raw.num_colors + 1
+        for i in range(number_of_planes):
+            echo.info(f'Extracting CFA plane #{i}')
+            mask = (self.raw.raw_colors_visible != i)
+            plane = np.copy(self.raw.raw_colors_visible)
+            plane[mask] = fill
+            yield plane
+
+    def get_num_of_colors(self):
+        return self.raw.num_colors
---
+++ echo.py
@@ -0,0 +1,58 @@
+"""Implements default stdout-like functions. """
+
+import click
+
+COLOR_SCHEMA = {
+    "ok": {
+        "fg": "green"
+    },
+    "warn": {
+        "fg": "yellow"
+    },
+    "error": {
+        "fg": "red"
+    },
+    "info": {
+        "fg": "blue"
+    }
+}
+
+
+def _colored_ok(msg):
+    """
+    Colored success output function.
+    """
+    msg = str(msg)
+    click.secho(msg, **COLOR_SCHEMA['ok'])
+
+
+def _colored_warn(msg):
+    """
+    Colored warning output function.
+    """
+    msg = str(msg)
+    click.secho(msg, **COLOR_SCHEMA['warn'])
+
+
+def _colored_error(msg):
+    """
+    Colored errors output function.
+    """
+    msg = str(msg)
+    click.secho(msg, **COLOR_SCHEMA['error'])
+
+
+def _colored_info(msg):
+    """
+    Colored info output function.
+    """
+    msg = str(msg)
+    click.secho(msg, **COLOR_SCHEMA['error'])
+
+
+ok = _colored_ok
+warn = _colored_warn
+error = _colored_error
+info = _colored_info
+out = click.echo
+
